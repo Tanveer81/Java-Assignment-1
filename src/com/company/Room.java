c478101eb62294844e71a16837d82b60c876cc08
Tanveer81 <tanvir01719@gmail.com>
2015-09-19 19:55:29 +0600

    done assignment

---
+++ src/com/company/Room.java
@@ -0,0 +1,46 @@
+package com.company;
+
+/**
+ * Created by Tanveer on 18-09-15.
+ */
+
+public class Room {
+    private String hallName;
+    private int roomNo;
+    private boolean status; // room allotment status
+    public Room(){
+        //do noting
+    }
+    public Room(String hall, int no){
+        hallName = hall;
+        roomNo = no;
+        status = false;
+    }
+    public String getHallName(){
+        return hallName;
+    }
+    public void setHallName (String hall){
+        hallName = hall;
+    }
+    public int getRoomNo(){
+        return roomNo;
+    }
+    public void setRoomNo(int no){
+        roomNo = no;
+    }
+    public boolean getStatus(){
+        return status;
+    }
+    public void setStatus (boolean state){
+        status = state;
+    }
+    public void print(){
+        if(getStatus()){
+            // cout << "Room No, " << getRoomNo() << " of " << getHallName() << " Hall is currently alloted."<<endl;
+            System.out.print("Room No, " + getRoomNo() + " of " + getHallName()+ " Hall is currently alloted. \n");
+        } else {
+            // cout << "Room No, " << getRoomNo() << " of " << getHallName() << " Hall is currently available for allotment."<<endl;
+            System.out.print("Room No, " + getRoomNo() + " of " + getHallName() + " Hall is currently available for allotment. \n");
+        }
+    }
+}
